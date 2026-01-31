2c60f04564551c83fcbc2cd1f005213adb97f9d6
louka2b <loukadeplace1@gmail.com>
2026-01-31 18:21:07 +0100

    start of the sound managing

--- src/fr/roomask/option_menu.gd
+++ src/fr/roomask/option_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var _master_bus_index = AudioServer.get_bus_index("Master")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,3 +19,15 @@ func _on_button_3_pressed() -> void:
 func easter_egg_on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/easter_egg.tscn")
 	
+
+
+func volume_slider(value: float) -> void:
+
+	var volume_db = linear_to_db(value)
+	
+	AudioServer.set_bus_volume_db(_master_bus_index, volume_db)
+	
+	if value == 0:
+		AudioServer.set_bus_mute(_master_bus_index, true)
+	else:
+		AudioServer.set_bus_mute(_master_bus_index, false)

1f4d0dd7631400886b7e410f367041ba49185863
louka2b <loukadeplace1@gmail.com>
2026-01-31 05:05:46 +0100

    easter egg

--- src/fr/roomask/option_menu.gd
+++ src/fr/roomask/option_menu.gd
@@ -5,11 +5,6 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
@@ -18,3 +13,7 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func easter_egg_on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/easter_egg.tscn")

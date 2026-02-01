7a74c7f398ef6d0ac3817c0f42dc888f2e04041b
louka2b <loukadeplace1@gmail.com>
2026-02-01 13:40:14 +0100

    add some animation on the menu

--- src/fr/roomask/main_menu.gd
+++ src/fr/roomask/main_menu.gd
@@ -1,24 +1,30 @@
 extends Control
 
+# Remplace les chemins ci-dessous par tes vrais fichiers
+@export var anim_lancement : SpriteFrames = preload("res://Assets/Main _menu/animation login.tres")
+@export var anim_bouton : SpriteFrames = preload("res://Assets/Main _menu/new_sprite_frames.tres")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	play_anim(anim_lancement)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func play_anim(ressource_frames : SpriteFrames):
+	var animated_sprite = AnimatedSprite2D.new()
+	animated_sprite.sprite_frames = ressource_frames
+	animated_sprite.position = size / 2
+	add_child(animated_sprite)
+	animated_sprite.play("default")
+	
+	await get_tree().create_timer(1.0).timeout
+	animated_sprite.queue_free()
 
 func _on_start_pressed() -> void:
-	print("press start")
+	await play_anim(anim_bouton) 
 	get_tree().change_scene_to_file("res://Scenes/room_1.tscn")
 
 func _on_option_pressed() -> void:
-	print("press option")
+	await play_anim(anim_bouton)
 	get_tree().change_scene_to_file("res://Scenes/option_menu.tscn")
 
 func _on_quit_pressed() -> void:
-	print("press quit")
+	await play_anim(anim_bouton)
 	get_tree().quit()
--- src/fr/roomask/option_menu.gd
+++ src/fr/roomask/option_menu.gd
@@ -1,12 +1,22 @@
 extends Control
 
 @onready var _master_bus_index = AudioServer.get_bus_index("Master")
+@export var anim_lancement : SpriteFrames = preload("res://Assets/Main _menu/animation login.tres")
+@export var anim_bouton : SpriteFrames = preload("res://Assets/Main _menu/new_sprite_frames.tres")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	play_anim(anim_lancement)
 
-
+func play_anim(ressource_frames : SpriteFrames):
+	var animated_sprite = AnimatedSprite2D.new()
+	animated_sprite.sprite_frames = ressource_frames
+	animated_sprite.position = size / 2
+	add_child(animated_sprite)
+	animated_sprite.play("default")
+	
+	await get_tree().create_timer(1.0).timeout
+	animated_sprite.queue_free()
+	
 func _on_h_slider_value_changed(value: float) -> void:
 	var volume_db = linear_to_db(value)
 	
@@ -19,4 +29,5 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	await play_anim(anim_bouton) 
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")

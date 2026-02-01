d7cd10e874e95a22b99d75311d1cf55960b10c7c
louka2b <loukadeplace1@gmail.com>
2026-02-01 15:37:59 +0100

    credit page added

--- src/fr/roomask/credit.gd
+++ src/fr/roomask/credit.gd
@@ -1,15 +1,17 @@
 extends Control
 
-@onready var _master_bus_index = AudioServer.get_bus_index("Master")
 @export var anim_lancement : SpriteFrames = preload("res://Assets/Main _menu/animation login.tres")
-				   
+
 func _ready() -> void:
 	play_anim(anim_lancement)
 
 func play_anim(ressource_frames : SpriteFrames):
 	var animated_sprite = AnimatedSprite2D.new()
 	animated_sprite.sprite_frames = ressource_frames
+	
+	animated_sprite.top_level = true
 	animated_sprite.position = get_viewport_rect().size / 2
+	
 	add_child(animated_sprite)
 	animated_sprite.play("default")
 	
@@ -19,29 +21,17 @@ func play_anim(ressource_frames : SpriteFrames):
 func play_anim2(ressource_frames : SpriteFrames):
 	var animated_sprite = AnimatedSprite2D.new()
 	animated_sprite.sprite_frames = ressource_frames
+	
+	animated_sprite.top_level = true
 	animated_sprite.position = get_viewport_rect().size / 2
+	
 	add_child(animated_sprite)
 	animated_sprite.play("end")
 	
 	await get_tree().create_timer(1.0).timeout
 	animated_sprite.queue_free()
-	
-func _on_h_slider_value_changed(value: float) -> void:
-	var volume_db = linear_to_db(value)
-	
-	AudioServer.set_bus_volume_db(_master_bus_index, volume_db)
-	
-	if value == 0:
-		AudioServer.set_bus_mute(_master_bus_index, true)
-	else:
-		AudioServer.set_bus_mute(_master_bus_index, false)
 
 
 func _on_button_pressed() -> void:
 	await play_anim2(anim_lancement)
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
-
-
-func _on_button_2_pressed() -> void:
-	await play_anim2(anim_lancement)
-	get_tree().change_scene_to_file("res://Scenes/credit.tscn")
+	get_tree().change_scene_to_file("res://Scenes/option_menu.tscn")
